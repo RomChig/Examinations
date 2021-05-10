5ad1917477afcfb8ede0d078779666e3240bee27
Roman Ihnatov <romamaster42@gmail.com>
2021-05-10 17:49:00 +0300

    deleted days from Timer.jsx

--- src/utils/Timer.jsx
+++ src/utils/Timer.jsx
@@ -17,7 +17,7 @@ const Timer = ({expiryTimestamp}) => {
         <div id = "timerBlock" className="float-end rounded border border-warning">
             <div style={{textAlign: 'center', color: 'white'}} id="timer">
                 <div style={{fontSize: '100px'}}>
-                    <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+                    <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
                 </div>
                 <p>Залишилося часу</p>
             </div>
